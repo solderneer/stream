f82fe56820cc15edba2c23885edc7dec6ccfefcb
Sudharshan <sudhar393@gmail.com>
2018-10-13 19:44:12 +0800

    added admin page

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Join from './views/Join.vue'
+import Admin from './views/Admin.vue'
 
 Vue.use(Router)
 
@@ -9,7 +11,12 @@ export default new Router({
     {
       path: '/',
       name: 'join',
-      component: Join
+      component: Join,
     },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: Admin,
+    }
   ]
 })
--- client/src/views/Join.vue
+++ client/src/views/Join.vue
@@ -1,69 +1,22 @@
 <template>
-    <div class="container">
-        <div class="form">
-            <logo/>
-            <h1>Join a session</h1>
-            <input-field v-model="sessionid" placeholder="Enter session ID"/>
-            <input-field v-model="nickname" placeholder="Enter nickname"/>
-            <div class="button-group">
-                <button-field>Admin mode</button-field>
-                <button-field>Submit</button-field>
-            </div>
-        </div>
-    </div>
+    <logon-page placeholder1="Enter session ID" 
+                placeholder2="Enter nickname" 
+                button1="Admin mode" 
+                button2="Submit"
+                header="Join a session"/>
 </template>
 
 <script>
-import Logo from '@/components/Logo.vue'
-import InputField from '@/components/InputField.vue'
-import ButtonField from '@/components/ButtonField.vue'
+import LogonPage from '@/components/LogonPage.vue'
 export default {
     name: 'Join',
-    data: function () {
-        return {
-            sessionid: '',
-            nickname: '',
-        }
-    },
     components: {
-        Logo,
-        InputField,
-        ButtonField,
+        LogonPage,
     }
 }
 </script>
 
-<style scoped>
-.container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);
-    height: 100vh;
-    width: 100vw;
-}
+<style>
 
-.form {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    
-    background-color: transparent;
-    width: 20%;
-    padding: 20px;
-}
-
-h1 {
-    color: white;
-    margin: 10px;
-    font-weight: 400;
-}
-
-.button-group {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    width: 100%;
-}
 </style>
+
