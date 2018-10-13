3101f71f7e7eb7f152464fcf0ef5d662c7f30a5d
Sudharshan <sudhar393@gmail.com>
2018-10-13 14:20:40 +0800

    docker stuff working

--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,13 @@
-const helloWorld = () => console.log('Hello!')
-helloWorld()
+import express from 'express'
+import http from 'http'
+
+let app = express()
+let server = http.Server(app)
+
+app.get('/', function (req, res) {
+  res.send('<h1>Hello!</h1>')
+})
+
+server.listen(3000, function () {
+  console.log('Listening on *:3000')
+})
