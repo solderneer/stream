bcb3da9bdbfe0e8d794abdb652c303a606f6b6e2
Sudharshan <sudhar393@gmail.com>
2018-10-14 12:11:36 +0800

    some architecture

--- server/src/index.js
+++ server/src/index.js
@@ -10,17 +10,30 @@ const io = socket(server, { origins: 'http://localhost:80' })
 const port = 3000
 
 const secret = 'password'
-
 const SUCCESS = 0
 const FAIL = 1
 
 let sessions = []
+let users = {}
+
+class UserType {
+  constructor (nickname, sessionid, isadmin) {
+    this.nickname = nickname
+    this.sessionid = sessionid
+    this.isadmin = isadmin
+  }
+}
 
 io.on('connection', function (socket) {
   console.log('Someone connected')
   socket.on('join', function (sessionid, nickname, callback) {
     let session = sessions.find((index) => { return index === sessionid })
     if (session) {
+      socket.join(session, () => {
+        users[socket.id] = new UserType(nickname, sessionid, false)
+        io.to(session).emit('connect', nickname + ' joined room ' + session)
+        console.log(users)
+      })
       callback(SUCCESS)
     } else {
       callback(FAIL)
@@ -28,14 +41,18 @@ io.on('connection', function (socket) {
   })
   socket.on('create', function (usecret, nickname, callback) {
     if (usecret === secret) {
-      sessions.push(randomstring.generate(7))
-      console.log('Admin connected')
-      console.log('New session ' + sessions[sessions.length - 1])
+      let sessionid = randomstring.generate(7)
+      sessions.push(sessionid)
+      users[socket.id] = new UserType(nickname, sessionid, true)
+      console.log('New session ' + sessionid)
       callback(SUCCESS)
     } else {
       console.log('Secret wrong')
       callback(FAIL)
     }
+  })
+  socket.on('disconnect', function () {
+    console.log(users[socket.id].nickname + ' disconnected')
   })
 })
 
