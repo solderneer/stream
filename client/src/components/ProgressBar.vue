2061e12cd7ccceeb16a9c455a02e2a38808229d8
Sudharshan <sudhar393@gmail.com>
2018-10-17 15:20:22 +0800

    adding dragger thingy

--- client/src/components/ProgressBar.vue
+++ client/src/components/ProgressBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="progressbar">
-    <progress :max="max" :value="value"></progress>
-    <div class="scrubber"></div>
+    <progress id="progress" :max="max" :value="value"></progress>
+    <div id="scrubber"></div>
   </div>
 </template>
 
@@ -13,14 +13,26 @@ export default {
     value: String,
   },
   mounted: function () {
+    let container = document.querySelector('.progressbar')
+    let progressbar = document.querySelector('#progress')
+    let scrubber = document.querySelector('#scrubber')
 
+    container.addEventListener('mouseover', function() {
+      scrubber.style.display = 'block'
+      container.style.margin = '0px 0px -18px 0px';
+    })
+
+    container.addEventListener('mouseout', function() {
+      scrubber.style.display = 'none'
+      container.style.margin = '0px 0px -2px 0px';
+    })
   },
 };
 </script>
 
 <style scoped>
 .progressbar {
-  margin-bottom: -18px;
+  margin-bottom: -2px;
 }
 progress[value] {
   -webkit-appearance: none;
@@ -28,13 +40,10 @@ progress[value] {
 
   width: 100%;
   height: 3px;
-
-  transition: height 0.5s;
 }
 
-progress[value]:hover {
+.progressbar:hover progress[value] {
   height: 5px;
-  transition: height 0.1s;
 }
 
 progress[value]::-webkit-progress-bar {
@@ -45,7 +54,8 @@ progress[value]::-webkit-progress-value {
   background-image: linear-gradient(120deg, rgba(132, 250, 175, 1) 0%, rgba(143, 210, 244, 1) 100%);
 }
 
-.scrubber {
+#scrubber {
+  display: none;
   position: relative;
   height: 15px;
   width: 15px;
@@ -53,7 +63,7 @@ progress[value]::-webkit-progress-value {
   background-color: rgba(143, 210, 244, 1);
 
   bottom: 12px;
-  left: 80%;
+  left: 79.5%;
 }
 </style>
 
